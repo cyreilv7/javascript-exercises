0c35584ccb8ff921e150060b77f7242a4b3e9a6a
cyreilv7 <shoiiru3@gmail.com>
2022-03-10 11:43:53 -0800

    Complete exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(fTemp) {
+  let cTemp = (fTemp - 32) * 5/9;
+  return Math.round(cTemp * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(cTemp) {
+  let fTemp = (cTemp * 9/5) + 32;
+  return Math.round(fTemp * 10) / 10; // 123.45 --> 1234.5 --> 1235 --> 123.5
 };
 
 // Do not edit below this line
